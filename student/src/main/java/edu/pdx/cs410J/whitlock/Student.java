84d0e272fc79ab9d60474ab428304707c86a71ae
David Whitlock <david.m.whitlock@gmail.com>
2020-06-24 20:54:42 -0700

    Store the gpa in a field.

--- student/src/main/java/edu/pdx/cs410J/whitlock/Student.java
+++ student/src/main/java/edu/pdx/cs410J/whitlock/Student.java
@@ -7,9 +7,11 @@ import java.util.ArrayList;
 /**                                                                                 
  * This class is represents a <code>Student</code>.                                 
  */                                                                                 
-public class Student extends Human {                                                
-                                                                                    
-  /**                                                                               
+public class Student extends Human {
+
+  private double gpa;
+
+  /**
    * Creates a new <code>Student</code>                                             
    *                                                                                
    * @param name                                                                    
@@ -24,6 +26,8 @@ public class Student extends Human {
    */                                                                               
   public Student(String name, ArrayList<String> classes, double gpa, String gender) {
     super(name);
+
+    this.gpa = gpa;
   }
 
   /**                                                                               
@@ -38,9 +42,9 @@ public class Student extends Human {
    * Returns a <code>String</code> that describes this                              
    * <code>Student</code>.                                                          
    */                                                                               
-//  public String toString() {
-//    return this.getName();
-//  }
+  public String toString() {
+    return this.getName() + " has a GPA of " + this.gpa;
+  }
 
   /**
    * Main program that parses the command line, creates a
