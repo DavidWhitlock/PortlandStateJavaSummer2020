3b571100786491f029859a5cc79fb537f53559a8
David Whitlock <david.m.whitlock@gmail.com>
2020-08-01 07:17:15 -0700

    Add the required Gradle configuration in order to use my Abstract classes in the Android project.

---
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/phonebill/PhoneCall.java
@@ -0,0 +1,25 @@
+package edu.pdx.cs410j.whitlock.phonebill;
+
+import edu.pdx.cs410J.AbstractPhoneCall;
+
+public class PhoneCall extends AbstractPhoneCall {
+    @Override
+    public String getCaller() {
+        return null;
+    }
+
+    @Override
+    public String getCallee() {
+        return null;
+    }
+
+    @Override
+    public String getStartTimeString() {
+        return null;
+    }
+
+    @Override
+    public String getEndTimeString() {
+        return null;
+    }
+}
