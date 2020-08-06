1f798af20bf62f5eef7387365b0ba15eb6c160a6
David Whitlock <david.m.whitlock@gmail.com>
2020-08-05 19:38:31 -0700

    Added the ability to pass the result of the CalculatorActivity back to the MainActivity.

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/phonebill/CalculatorActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/phonebill/CalculatorActivity.java
@@ -2,13 +2,17 @@ package edu.pdx.cs410j.whitlock.phonebill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity {
+
+    private Double sum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +29,21 @@ public class CalculatorActivity extends AppCompatActivity {
                 double left = Double.parseDouble(leftOperand.getText().toString());
                 double right = Double.parseDouble(rightOperand.getText().toString());
 
-                double sum = left + right;
+                sum = left + right;
 
                 TextView result = findViewById(R.id.result);
                 result.setText(String.valueOf(sum));
+            }
+        });
 
+        Button backToMain = findViewById(R.id.backToMain);
+        backToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CalculatorActivity.this, MainActivity.class);
+                intent.putExtra("Sum", sum);
+                setResult(RESULT_OK, intent);
+                finish();
             }
         });
     }
--- phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/phonebill/MainActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/phonebill/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -19,6 +20,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int CALCULATOR_RESULT = 43;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +32,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, CALCULATOR_RESULT);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == CALCULATOR_RESULT && resultCode == RESULT_OK) {
+            if (data != null && data.hasExtra("Sum")) {
+                double result = data.getDoubleExtra("Sum", 0.0);
+                Toast.makeText(this, "Result was " + result, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
