973a3158c0d53a6218179c217ee5ca869a1b9a36
David Whitlock <david.m.whitlock@gmail.com>
2020-08-05 19:11:27 -0700

    Actually have the CalculatorActivity do something.

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/phonebill/CalculatorActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/whitlock/phonebill/CalculatorActivity.java
@@ -3,6 +3,10 @@ package edu.pdx.cs410j.whitlock.phonebill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        Button execute = findViewById(R.id.execute);
+        execute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText leftOperand = findViewById(R.id.leftOperand);
+                EditText rightOperand = findViewById(R.id.rightOperand);
+
+                double left = Double.parseDouble(leftOperand.getText().toString());
+                double right = Double.parseDouble(rightOperand.getText().toString());
+
+                double sum = left + right;
+
+                TextView result = findViewById(R.id.result);
+                result.setText(String.valueOf(sum));
+
+            }
+        });
     }
 }
